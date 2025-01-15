e120321e070246294b8da7981a7a9a8bbf1be959
Helmi <helmi.laakkonen@gmail.com>
2025-01-15 17:29:11 +0100

    initial commit

---
+++ project-management-ap/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Footer></Footer>
+  );
+}
+
+export default App;
---
+++ project-management-ap/src/components/Footer.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function Footer() {
+  return (
+    <div className="">
+        <Button variant="contained">Hello world</Button>
+    </div>
+  );
+}
+
+export default Footer;
