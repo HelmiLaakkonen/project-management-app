feef1f8f9f2f46e1dff66ed5ce7fa99ce55bfc80
Helmi Laakkonen <helmi.laakkonen@gmail.com>
2025-03-06 12:05:01 +0200

    added POST for tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,13 +2,33 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/connection");
 
-router.get('/tasks', (req, res) => {
-  db.query('SELECT * FROM tasks', (err, results) => {
+router.get("/tasks", (req, res) => {
+  db.query("SELECT * FROM tasks", (err, results) => {
     if (err) {
-      console.error('Database error:', err);
-      return res.status(500).json({ error: 'Database query failed' });
+      console.error("Database error:", err);
+      return res.status(500).json({ error: "Database query failed" });
     }
     res.json({ tasks: results });
+  });
+});
+
+router.post("/tasks", (req, res) => {
+  const { task_name, description, status } = req.body;
+
+  if (!task_name || !status) {
+    return res.status(400).json({ error: "Task name and status are required" });
+  }
+
+  const query = `INSERT INTO tasks (task_name, description, status) VALUES (?, ?, ?)`;
+
+  db.execute(query, [task_name, description, status], (err, results) => {
+    if (err) {
+      console.error(("Error adding task: ", err));
+      return res.status(500).json({ error: "Database error" });
+    }
+    res
+      .status(201)
+      .json({ message: "Task added succesfully", task_id: results.insertId });
   });
 });
 
